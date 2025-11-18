475722f85fcb2b863e7fe307adb653082333b644
jun <pickjog@gmail.com>
2025-11-18 19:29:42 +0900

    feat(event): 인벤토리 이벤트 퍼블리서 인터페이스 및 프로듀서

---
+++ src/main/java/com/early_express/inventory_service/domain/inventory/domain/messaging/InventoryEventPublisher.java
@@ -0,0 +1,25 @@
+package com.early_express.inventory_service.domain.inventory.domain.messaging;
+
+import com.early_express.inventory_service.domain.inventory.domain.model.Inventory;
+
+/**
+ * Inventory 이벤트 발행 포트 (도메인 인터페이스)
+ * - Infrastructure 계층에서 구현
+ */
+public interface InventoryEventPublisher {
+
+    /**
+     * 재고 생성 이벤트 발행
+     */
+    void publishInventoryCreated(Inventory inventory);
+
+    /**
+     * 재고 부족 이벤트 발행
+     */
+    void publishInventoryLowStock(Inventory inventory);
+
+    /**
+     * 재입고 이벤트 발행
+     */
+    void publishInventoryRestocked(Inventory inventory, Integer restockedQuantity);
+}
