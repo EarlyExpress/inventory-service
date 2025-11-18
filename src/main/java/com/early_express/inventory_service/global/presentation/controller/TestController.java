f1537e02686b48927e43e66c169f8de573751d54
jun <pickjog@gmail.com>
2025-11-18 14:02:25 +0900

    feat(exception): inventory 도메인 예외 코드 및 예외 구성

--- src/main/java/com/early_express/inventory_service/global/presentation/controller/TestController.java
+++ src/main/java/com/early_express/inventory_service/global/presentation/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.early_express.inventory_service.global.presentation.controller;
 
-import com.early_express.product_service.global.infrastructure.security.UserDetailsImpl;
+import com.early_express.inventory_service.global.infrastructure.security.UserDetailsImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
